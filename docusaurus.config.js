84d818f066ee7e47d26f2a0d300b179a8fb09f8e
liwenkai <2020583117@qq.com>
2023-03-24 17:31:16 +0800

    暂时提交

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -42,14 +42,14 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
+            "https://github.com/liwenka1/my-website/blob/main",
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
+            "https://github.com/liwenka1/my-website/blob/main",
         },
         theme: {
           customCss: require.resolve("./src/css/custom.css"),
